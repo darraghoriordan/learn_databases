09b1f8075e2be7b764fa61a5cf22d8735dbf0f92
Darragh ORiordan <darragh.oriordan@gmail.com>
2022-06-17 10:39:25 +1000

    add timeout for jest

--- src/testing/preRun.ts
+++ src/testing/preRun.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 
 import AppDataSource from "../database-connection/appDatasource";
-
+jest.setTimeout(1_000_000);
 afterAll(async () => {
   await AppDataSource.closeConnections();
 });
